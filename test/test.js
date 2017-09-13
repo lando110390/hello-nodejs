2db13cda6feef4152a93316aad9ba07ae315af94
lando110390 <lando110390@gmail.com>
2017-09-13 19:47:02 +0700

    update code

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-      request(app).get('/').expect('!!! Welcome to Nodejs Server !!!', done);
+      request(app).get('/').expect('!!! How are you today !!!', done);
     });
 });
